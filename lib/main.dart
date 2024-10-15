7e61da72f6d519999cb3a9e721bcb658db6ccf54
lusca0x01 <lucas.wf@outlook.com>
2024-10-15 11:39:34 -0300

    refact: Refact navigation and ensure that the widgets were initialized

--- lib/main.dart
+++ lib/main.dart
@@ -4,13 +4,19 @@ import 'package:tractian_mobile_challenge/features/assets/assets.dart';
 import 'package:tractian_mobile_challenge/features/home/home.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
   await Config.init();
 
   runApp(MaterialApp(
     initialRoute: "/",
     routes: {
-      "/": (context) => const HomePage(),
-      "/assets": (context) => const AssetsPage(),
+      "/": (_) => const HomePage(),
+      "/assets": (context) {
+        final arguments =
+            ModalRoute.of(context)?.settings.arguments as Map<String, dynamic>?;
+        return AssetsPage(id: arguments!['id']);
+      }
     },
     debugShowCheckedModeBanner: false,
   ));
