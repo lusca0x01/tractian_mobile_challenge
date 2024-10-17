bb522dbe66f01a261320e79777d93a11adc3e053
lusca0x01 <lucas.wf@outlook.com>
2024-10-17 00:56:28 -0300

    feat: Add parent reference, number of visibleChildren (to help with UI performance) and methods to compare TreeNodes

--- lib/core/tree/tree_node.dart
+++ lib/core/tree/tree_node.dart
@@ -7,18 +7,49 @@ class TreeNode {
     required this.value,
     this.isCritical = false,
     this.isEnergy = false,
+    this.visibleChildrenCount = 3,
   });
 
   final String id;
   final bool isCritical;
   final bool isEnergy;
+  TreeNode? parent;
   final List<TreeNode> children = [];
   final NodeTypes type;
   final String value;
+  int visibleChildrenCount;
+  bool get isLeaf => children.isEmpty;
 
   void addChild(TreeNode child) {
+    child.parent = this;
     children.add(child);
   }
 
-  bool get isLeaf => children.isEmpty;
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+
+    if (other is! TreeNode) return false;
+
+    return (id == other.id &&
+        isCritical == other.isCritical &&
+        isEnergy == other.isEnergy &&
+        parent == other.parent &&
+        children == other.children &&
+        type == other.type &&
+        value == other.value &&
+        isLeaf == other.isLeaf &&
+        hashCode == other.hashCode);
+  }
+
+  @override
+  int get hashCode => Object.hash(
+        id,
+        isCritical,
+        isEnergy,
+        parent,
+        children,
+        type,
+        value,
+      );
 }
