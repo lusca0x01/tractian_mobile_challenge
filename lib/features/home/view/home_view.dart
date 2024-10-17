fafdc8685825c0f1de97d22bc373f07cd4719730
lusca0x01 <lucas.wf@outlook.com>
2024-10-17 00:54:03 -0300

    refact: Refact UI way to handle the distance between the cards and CircularProgressIndicator

--- lib/features/home/view/home_view.dart
+++ lib/features/home/view/home_view.dart
@@ -21,7 +21,15 @@ class HomeView extends StatelessWidget {
       ),
       body: BlocBuilder<HomeBloc, HomeState>(builder: (context, state) {
         if (state.isLoading) {
-          return const Center(child: CircularProgressIndicator());
+          return const Center(
+            child: SizedBox(
+              height: 64,
+              width: 64,
+              child: CircularProgressIndicator(
+                color: ChallengeColors.blue,
+              ),
+            ),
+          );
         }
         if (state.companiesList.isEmpty && !state.isLoading) {
           return Center(
@@ -34,58 +42,60 @@ class HomeView extends StatelessWidget {
                 ),
                 Text(
                   "Sem Internet",
-                  style: ChallengeTypography.regularLG(color: Colors.black),
+                  style: ChallengeTypography.regularLG(
+                      color: ChallengeColors.black),
                 ),
               ],
             ),
           );
         }
-        return ListView.separated(
-          padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 22),
+        return ListView.builder(
+          padding: const EdgeInsets.symmetric(vertical: 22, horizontal: 22),
+          physics: const BouncingScrollPhysics(),
           itemCount: state.companiesList.length,
           itemBuilder: (context, index) {
-            return Card(
-              clipBehavior: Clip.hardEdge,
-              color: ChallengeColors.blue,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(5),
-              ),
-              child: InkWell(
-                splashColor: ChallengeColors.blue.withAlpha(30),
-                onTap: () {
-                  Navigator.pushNamed(context, '/assets',
-                      arguments: {"id": state.companiesList[index].id!}).then(
-                    (_) {
-                      if (context.mounted) {
-                        context
-                            .read<HomeBloc>()
-                            .add(const FetchCompaniesEvent());
-                      }
-                    },
-                  );
-                },
-                child: SizedBox(
-                  height: size.height * 0.12,
-                  child: Row(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(left: 32, right: 16),
-                        child: ChallengeIcons.company(),
-                      ),
-                      Text(
-                        state.companiesList[index].name!,
-                        style: ChallengeTypography.mediumLG(
-                            color: ChallengeColors.white),
-                      ),
-                    ],
+            return Padding(
+              padding: const EdgeInsets.only(bottom: 22.0),
+              child: Card(
+                clipBehavior: Clip.hardEdge,
+                color: ChallengeColors.blue,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(5),
+                ),
+                child: InkWell(
+                  splashColor: ChallengeColors.blue.withAlpha(30),
+                  onTap: () {
+                    Navigator.pushNamed(context, '/assets',
+                        arguments: {"id": state.companiesList[index].id!}).then(
+                      (_) {
+                        if (context.mounted) {
+                          context
+                              .read<HomeBloc>()
+                              .add(const FetchCompaniesEvent());
+                        }
+                      },
+                    );
+                  },
+                  child: SizedBox(
+                    height: size.height * 0.10,
+                    child: Row(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(left: 32, right: 16),
+                          child: ChallengeIcons.company(),
+                        ),
+                        Text(
+                          state.companiesList[index].name!,
+                          style: ChallengeTypography.mediumLG(
+                              color: ChallengeColors.white),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ),
             );
           },
-          separatorBuilder: (BuildContext context, int index) => SizedBox(
-            height: size.height * 0.05,
-          ),
         );
       }),
     );
