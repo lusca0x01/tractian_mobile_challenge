a61f9e3f0db17e2c13113049f2e0e50b823b0deb
lusca0x01 <lucas.wf@outlook.com>
2024-10-17 01:08:03 -0300

    fix: Fix tree node widget and tree view problem with perfomance and implement a way to paginate the ExpansionTiles in the UI

--- lib/features/assets/view/widgets/tree_node_widget.dart
+++ lib/features/assets/view/widgets/tree_node_widget.dart
@@ -6,40 +6,66 @@ import 'package:tractian_mobile_challenge/theme/colors.dart';
 import 'package:tractian_mobile_challenge/theme/icons.dart';
 import 'package:tractian_mobile_challenge/theme/typography.dart';
 
-class TreeNodeWidget extends StatelessWidget {
+class TreeNodeWidget extends StatefulWidget {
   final TreeNode node;
+  final bool initiallyExpanded;
+  final bool isBig;
 
-  const TreeNodeWidget({super.key, required this.node});
+  const TreeNodeWidget({
+    super.key,
+    required this.node,
+    required this.initiallyExpanded,
+    required this.isBig,
+  });
 
   @override
+  State<TreeNodeWidget> createState() => _TreeNodeWidgetState();
+}
+
+class _TreeNodeWidgetState extends State<TreeNodeWidget> {
+  @override
   Widget build(BuildContext context) {
+    final node = widget.node;
+    final initiallyExpanded = widget.initiallyExpanded;
+    final isBig = widget.isBig;
+
     return ExpansionTile(
-      title: Row(children: [
-        _getNodeIcon(node.type),
-        const SizedBox(width: 6.0),
-        Flexible(
-          child: Text(
-            node.value,
-            style: ChallengeTypography.mediumSM(color: Colors.black),
-            overflow: TextOverflow.ellipsis,
-            maxLines: 1,
-          ),
-        ),
-        const SizedBox(width: 6.0),
-        if (node.isEnergy)
-          ChallengeIcons.energyFilled(color: ChallengeColors.green),
-        if (node.isCritical)
-          Container(
-            width: 12,
-            height: 12,
-            decoration: const BoxDecoration(
-              color: ChallengeColors.red,
-              shape: BoxShape.circle,
+      key: ValueKey("${node.id}.${widget.initiallyExpanded}"),
+      title: Row(
+        children: [
+          _getNodeIcon(node.type),
+          const SizedBox(width: 4.0),
+          Flexible(
+            child: Text(
+              node.value,
+              style: ChallengeTypography.mediumSM(color: ChallengeColors.black),
+              overflow: TextOverflow.ellipsis,
+              maxLines: 1,
             ),
-          )
-      ]),
+          ),
+          const SizedBox(width: 4.0),
+          if (node.isEnergy)
+            ChallengeIcons.energyFilled(
+              color: ChallengeColors.green,
+              width: 12,
+              height: 12,
+            ),
+          if (node.isCritical)
+            Container(
+              width: 12,
+              height: 12,
+              decoration: const BoxDecoration(
+                color: ChallengeColors.red,
+                shape: BoxShape.circle,
+              ),
+            ),
+        ],
+      ),
+      maintainState: !node.isLeaf && initiallyExpanded,
+      initiallyExpanded: initiallyExpanded,
       controlAffinity: ListTileControlAffinity.leading,
       leading: node.isLeaf ? const SizedBox.shrink() : null,
+      iconColor: ChallengeColors.blue,
       tilePadding: const EdgeInsets.only(left: 4.0),
       enabled: !node.isLeaf,
       childrenPadding: const EdgeInsets.only(left: 16.0),
@@ -49,16 +75,41 @@ class TreeNodeWidget extends StatelessWidget {
       shape: const RoundedRectangleBorder(
         side: BorderSide.none,
       ),
-      children: [
-        ListView.builder(
-          shrinkWrap: true,
-          physics: const NeverScrollableScrollPhysics(),
-          itemCount: node.children.length,
-          itemBuilder: (context, index) {
-            return TreeNodeWidget(node: node.children[index]);
-          },
-        ),
-      ],
+      children: initiallyExpanded && isBig
+          ? [
+              ...node.children.take(node.visibleChildrenCount).map((childNode) {
+                return TreeNodeWidget(
+                  node: childNode,
+                  initiallyExpanded: initiallyExpanded,
+                  isBig: isBig,
+                );
+              }),
+              if (node.visibleChildrenCount < node.children.length)
+                ListTile(
+                  title: TextButton(
+                    onPressed: () {
+                      setState(() {
+                        node.visibleChildrenCount += 10;
+                        if (node.visibleChildrenCount > node.children.length) {
+                          node.visibleChildrenCount = node.children.length;
+                        }
+                      });
+                    },
+                    child: Text(
+                      'Carregar mais (${node.children.length - node.visibleChildrenCount})',
+                      style: ChallengeTypography.mediumSM(
+                          color: ChallengeColors.blue),
+                    ),
+                  ),
+                ),
+            ]
+          : node.children.map((childNode) {
+              return TreeNodeWidget(
+                node: childNode,
+                initiallyExpanded: initiallyExpanded,
+                isBig: isBig,
+              );
+            }).toList(),
     );
   }
 
@@ -71,7 +122,7 @@ class TreeNodeWidget extends StatelessWidget {
       case NodeTypes.asset:
         return ChallengeIcons.asset(color: ChallengeColors.blue);
       default:
-        throw "Error";
+        throw Exception("No provided icon error");
     }
   }
 }
--- lib/features/assets/view/widgets/tree_view.dart
+++ lib/features/assets/view/widgets/tree_view.dart
@@ -1,19 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:tractian_mobile_challenge/core/tree/tree.dart';
+import 'package:tractian_mobile_challenge/features/assets/bloc/assets_bloc.dart';
 import 'package:tractian_mobile_challenge/features/assets/view/widgets/tree_node_widget.dart';
 
 class TreeView extends StatelessWidget {
-  final Tree tree;
+  const TreeView({
+    super.key,
+    required this.tree,
+  });
 
-  const TreeView({super.key, required this.tree});
+  final Tree tree;
 
   @override
   Widget build(BuildContext context) {
+    final state = context.watch<AssetsBloc>().state;
+    final shouldExpand = (state.energySensorFilter ||
+        state.criticalFilter ||
+        state.searchTextFilter.isNotEmpty);
+
     return ListView.builder(
-      shrinkWrap: true,
-      itemCount: tree.nodes.length,
+      physics: const BouncingScrollPhysics(),
+      itemCount: tree.rootNodes.length,
       itemBuilder: (context, index) {
-        return TreeNodeWidget(node: tree.nodes[index]);
+        return TreeNodeWidget(
+          node: tree.rootNodes[index],
+          initiallyExpanded: shouldExpand,
+          isBig: tree.getSize() > 100,
+        );
       },
     );
   }
